3a01f1c6256c54e6a7653bf2a9d4e13d884f4910
sandipan-das-sd <sd901656@gmail.com>
2025-11-25 17:58:11 +0530

    Update API URL and CORS origin for production environment

--- server/src/index.js
+++ server/src/index.js
@@ -34,7 +34,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(morgan('dev'));
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+  origin: ['http://localhost:5173', 'http://127.0.0.1:5173',"https://trade.gyanoda.in","https://nifty.gyanoda.in"],
   credentials: true,
 }));
 
