fd49cc56dae36e13587cc7bac7be7a68bffcfa50
sandipan-das-sd <sd901656@gmail.com>
2026-03-20 10:44:10 +0530

    feat: Update CORS configuration to include new domain for API access

--- server/src/index.js
+++ server/src/index.js
@@ -57,7 +57,7 @@ serverEvents.setMaxListeners(50);
 // Middleware
 // CORS must come before other middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://trade.gyanoda.in', 'https://nifty.gyanoda.in'],
+  origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://trade.gyanoda.in', 'https://nifty.gyanoda.in', 'https://ema.gyanoda.in'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
